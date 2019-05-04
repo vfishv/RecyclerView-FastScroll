76e340ec6eaa118fd6dc175ac63afdd67b00273f
Quentin <vFISHv@gmail.com>
2019-05-05 02:49:41 +0800

    up

--- sample/src/main/java/com/simplecityapps/recyclerview_fastscroll/sample/activity/MainActivity.java
+++ sample/src/main/java/com/simplecityapps/recyclerview_fastscroll/sample/activity/MainActivity.java
@@ -19,11 +19,11 @@ package com.simplecityapps.recyclerview_fastscroll.sample.activity;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.simplecityapps.recyclerview_fastscroll.sample.R;
 import com.simplecityapps.recyclerview_fastscroll.sample.fragment.MultiViewTypeFragment;
